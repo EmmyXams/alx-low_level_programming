8d73c7ea06cf0deaf0546408eae817c21c322111
EmmyXams <trustonlinetech@gmail.com>
2023-09-07 21:06:42 -0700

    adding all mandatory files for 0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print string using puts
+ * description - Write a C program that prints exactly
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print string using printf
+ * description - Write a C program that prints exactly
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
